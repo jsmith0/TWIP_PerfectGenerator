5e35b9c88a4e1cbccebb011c892e92440fc3c08f
jsmith0 <justins4@comcast.net>
2016-10-11 08:06:33 -0400

    work in progress

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,29 +4,29 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	    Scanner kb = new Scanner(System.in);
-        System.out.println("Enter your name.");
-        String name1 = kb.next();
-        System.out.println("Nice to meet you, " + name1 + ". Your lucky number is " + perfectRandom.perfsqr());
+   public static int var = 0;
+    public static Object[] names = new Object [100];
 
-        System.out.println("Type \"0\" to end the code. \nType \"1\" to see a list of all of the prior entries. \n" +
-                "Type \"2\" to enter another name.");
-        String response = kb.next();
-        if (response == "0")
+    public static void main(String[] args) {
+        while(true)
         {
-            //how do i have the program cut off?????
-            System.exit(0);
+            Scanner kb = new Scanner(System.in);
+            System.out.println("Enter your name.");
+            String name1 = kb.next();
+            System.out.println("Nice to meet you, " + name1 + ". Your lucky number is " + perfectRandom.perfsqr());
+
+            System.out.println("Type \"0\" to end the code. \nType \"1\" to see a list of all of the prior entries. \n" +
+                    "Type \"2\" to enter another name.");
+            int response = kb.nextInt();
+            if (response == 0) {
+                //how do i have the program cut off?????
+                break;
+            } else if (response == 1) {
+                var++;
+                        names[var] = object;
+                break;
+            } else if (response == 2)
+                System.out.println("Type in another name");
+            }
         }
-        else if (response == "1")
-        {
-            //gotta figure this one out  O.o
-            System.out.println(name1 + "" + perfectRandom.perfsqr());
-        }
-        else if ( response == "2")
-        {
-            //and i gotta figure this one out too.......
-            System.out.println("Type in another name");
-        }
-    }
 }
