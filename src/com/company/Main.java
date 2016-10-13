f2bb1021945be450f3207159791b751b6bfefa2a
jsmith0 <justins4@comcast.net>
2016-10-13 08:12:37 -0400

    work in progress

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,29 +4,37 @@ import java.util.Scanner;
 
 public class Main {
 
-   public static int var = 0;
     public static Object[] names = new Object [100];
-
+    public static int times=0;
     public static void main(String[] args) {
+        outerloop:
         while(true)
         {
             Scanner kb = new Scanner(System.in);
             System.out.println("Enter your name.");
-            String name1 = kb.next();
-            System.out.println("Nice to meet you, " + name1 + ". Your lucky number is " + perfectRandom.perfsqr());
+            String name1 = kb.next().trim();
+            int sto = perfectRandom.perfsqr();
+            System.out.println("Nice to meet you, " + name1 + ". Your lucky number is " + perfectRandom.perfsqr() + ".\n");
 
-            System.out.println("Type \"0\" to end the code. \nType \"1\" to see a list of all of the prior entries. \n" +
-                    "Type \"2\" to enter another name.");
-            int response = kb.nextInt();
-            if (response == 0) {
-                //how do i have the program cut off?????
-                break;
-            } else if (response == 1) {
-                var++;
-                        names[var] = object;
-                break;
-            } else if (response == 2)
-                System.out.println("Type in another name");
-            }
+            names[times]=new String(name1+" your number is "+sto);
+            times++;
+           while(true) {
+               System.out.println("Type \"0\" to end the code. Type \"1\" to see a list of all of the prior entries. Type \"2\" to enter another name.");
+               int response = kb.nextInt();
+               if (response == 0) {
+                   break outerloop;
+               } else if (response == 1) {
+                   for (Object x : names) {
+                       if (x != null) {
+                           System.out.println(x.toString());
+                       }
+                   }
+                   break;
+               } else if (response != 0 || response != 1 || response != 2) {
+                   System.out.println("Please enter a valid number.");
+               }
+           }
+
         }
+    }
 }
--- src/com/company/perfectRandom.java
+++ src/com/company/perfectRandom.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class perfectRandom {
     public static int perfsqr ()
     {
-      int c = (int)(Math.random()*13);
+      int c = (int)(Math.random()*25);
         c = c*c;
         int cout = c;
         return cout;
